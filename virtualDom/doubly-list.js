8823967d9e247022a149da6021581d0f6a9a9d23
Marc Dwyer <marcdwyer1738@gmail.com>
2019-12-26 16:42:48 -0600

    something

--- virtualDom/doubly-list.js
+++ virtualDom/doubly-list.js
@@ -2,15 +2,13 @@ class FiberNode {
   constructor(fiber) {
     console.log(fiber);
     this.fiber = fiber;
-    this.childrenArray = fiber.children;
-    this.child = null;
-    this.sibling = null;
+    this.child = {};
+    this.sibling = {};
     this.parent = null;
   }
 }
 
 function link(parent1, node) {
-  // console.log(node);
   const parent = new FiberNode(parent1);
   let index = 0;
   let current = node[index];
@@ -20,7 +18,7 @@ function link(parent1, node) {
     const { childrenArray } = fiberNode;
     fiberNode.parent = parent;
     if (childrenArray.length) {
-      fiberNode = link(current, fiberNode.childrenArray);
+      fiberNode.child = link(current, fiberNode.childrenArray);
     }
     if (prevNode) fiberNode.sibling = prevNode;
     prevNode = fiberNode;
